4ae7268707a2f3603603e15526603bba47871eb8
Ko2m0 <kozmopimp@gmail.com>
2023-01-19 19:08:56 -0600

    updates images in card

--- resources/views/bizandbreakfast/show.blade.php
+++ resources/views/bizandbreakfast/show.blade.php
@@ -27,7 +27,9 @@
                     <h5 class="trescolor-txt">{{$bandb_profile['profile_job']}}</h5>
                 </div>
                 <div class="flexxer wrapit align-center just-center" style="padding: 10px 0">
-                    <a href="{{$bandb_profile['profile_ws']}}" class="ws-header2" target="_blank"></a>
+                    <a href="{{$bandb_profile['profile_ws']}}" class="ws-header2" target="_blank">
+                    <img src="https://www.connectusnw.com/profiles_img/assets/Connextus_websiteLogo.svg" alt="Website">
+                    </a>
                     <div class="clear"></div>
                 </div>
 
@@ -41,20 +43,20 @@
             <a href="sms:{{$bandb_profile['profile_phonenumber']}}">   
             <!--<a href="https://wa.me//?text=Hello%20I%20would%20like%20to%20contact%20you">-->
                     <div class="k-btn">
-                        <img src="https://dev.connextusnow.com/images/svg/msg_icon.svg" alt="Message">
+                        <img src="https://www.connectusnw.com/profiles_img/assets/msg_icon.svg" alt="Message">
                     </div>
                     <p class="center-text uppercase unuscolor-txt bold">Message</p>
                 </a>
             </div>
             <div class="one-third">
                 <div class="k-btn" id="MeetBtn">
-                    <img src="https://dev.connextusnow.com/images/svg/meet_icon.svg" alt="Meeting">
+                    <img src="https://www.connectusnw.com/profiles_img/assets/meet_icon.svg" alt="Meeting">
                 </div>
                 <p class="center-text uppercase unuscolor-txt bold">Meeting</p>
             </div>
             <div class="one-third last-column">
                     <div class="k-btn" id="ReferBtn">
-                        <img src="https://dev.connextusnow.com/images/svg/refer_icon.svg" alt="Referral">
+                        <img src="https://www.connectusnw.com/profiles_img/assets/refer_icon.svg" alt="Referral">
                     </div>
                 <p class="center-text uppercase unuscolor-txt bold">Referral</p>
             </div>
@@ -67,13 +69,13 @@
         <a href="tel:{{$bandb_profile['profile_phonenumber']}}" class="button-big duo-bg phone-btn">
             <div class="flexxer just-center">
                 <div class="callbtn_icon">
-                    <img src="https://dev.connextusnow.com/images/ux/Connextus_call_phoneIcon1.svg" alt="Call" width="30">
+                    <img src="https://www.connectusnw.com/profiles_img/assets/Connextus_call_phoneIcon1.svg" alt="Call" width="30">
                 </div>
                 <div class="callbtn_icon">
-                    <img src="https://dev.connextusnow.com/images/ux/connextus_textlogoW.svg" alt="ConneXtus" width="150">
+                    <img src="https://www.connectusnw.com/profiles_img/assets/connextus_textlogoW.svg" alt="ConneXtus" width="150">
                 </div>
                 <div class="callbtn_icon">
-                    <img src="https://dev.connextusnow.com/images/ux/Connextus_logoSmall_whiteborder1.svg" alt="Logo" width="30" style="border: 2px solid #ffffff; border-radius:5px">
+                    <img src="https://www.connectusnw.com/profiles_img/assets/Connextus_logoSmall_whiteborder1.svg" alt="Logo" width="30" style="border: 2px solid #ffffff; border-radius:5px">
                 </div>
             </div>
         </a>
@@ -85,20 +87,20 @@
             <div class="flexxer just-around align-center">
                 <a href="https://www.linkedin.com/shareArticle?mini=true&url=card.connextusnow.com%2F" target="_blank"
                     class="">
-                    <img src="https://dev.connextusnow.com/images/sm/Connextus_linkedinLogo.svg"
+                    <img src="https://www.connectusnw.com/profiles_img/assets/Connextus_linkedinLogo.svg"
                         alt="Linkedin" width="20px">
                 </a>
                 <a href="" target="_blank" class="">
-                    <img src="https://dev.connextusnow.com/images/sm/Connextus_instagramLogo.svg" alt=""
+                    <img src="https://www.connectusnw.com/profiles_img/assets/Connextus_instagramLogo.svg" alt="Instagram"
                         width="20px">
                 </a>
                 <a href="https://www.facebook.com/sharer.php?u=card.connextusnow.com%2F"
                     target="_blank" class="">
-                    <img src="https://dev.connextusnow.com/images/sm/Connextus_facebookLogo.svg" alt=""
+                    <img src="https://www.connectusnw.com/profiles_img/assets/Connextus_facebookLogo.svg" alt="Facebook"
                         width="20px">
                 </a>
                 <a href="https://twitter.com/intent/tweet?text=ConneXtusisagreattooltomeetbusinessaquantancies!&url=alt.connextusnow.com" target="_blank">
-                    <img src="https://dev.connextusnow.com/images/sm/Connextus_twitterLogo.svg" alt=""
+                    <img src="https://www.connectusnw.com/profiles_img/assets/Connextus_twitterLogo.svg" alt="Twitter"
                         width="25px">
                 </a>
                 
--- resources/views/partials/cardheader.blade.php
+++ resources/views/partials/cardheader.blade.php
@@ -1,7 +1,7 @@
 <header class="videoSection">
     <div class="container videocontainer">
       <video playsinline controls class="responsive-video"  
-        poster="http://www.connextusnow.com/connextus_app/img/connextus_thumbnail2.jpg">
+        poster="https://www.connectusnw.com/profiles_img/assets/connextus_thumbnail2.jpg">
         <source src="{{$bandb_profile['profile_video']}}" type="video/mp4">
              Your browser does not support the video tag.
       </video>
